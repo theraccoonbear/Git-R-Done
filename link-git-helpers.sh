3b776da2559081b5aa75f6c0cfaa394aeae857c0
Don Smith <don.c.smith@gmail.com>
2017-04-14 21:24:56 -0500

    gitactivity

---
+++ link-git-helpers.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln `pwd`/gitpush ~/bin/gitpush
+ln `pwd`/gitmerge ~/bin/gitmerge
+ln `pwd`/gitpreview ~/bin/gitpreview
+ln `pwd`/gitparent ~/bin/gitparent
+ln `pwd`/git-hotfix-merges ~/bin/git-hotfix-merges
+
